d810cf88889cf701960640b0563002ca23492e4d
d-otynshin <daniyar.otynshin@gmail.com>
2025-03-08 20:40:00 +0500

    feat: add prefix for gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -3,6 +3,7 @@ import { ApiGatewayModule } from './api-gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
+  app.setGlobalPrefix('/api');
   await app.listen(process.env.port ?? 3000);
 }
-bootstrap();
+void bootstrap();
