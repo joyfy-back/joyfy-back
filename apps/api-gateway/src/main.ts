e68750a60a38931e5fad9eec480ac06fddb79570
d-otynshin <daniyar.otynshin@gmail.com>
2025-03-08 22:03:12 +0500

    feat: add version prefix

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -3,7 +3,7 @@ import { ApiGatewayModule } from './api-gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
-  app.setGlobalPrefix('/api');
+  app.setGlobalPrefix('/api/v1');
   await app.listen(process.env.port ?? 3000);
 }
 void bootstrap();
