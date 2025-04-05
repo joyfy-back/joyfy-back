e616211b5ea47cb23fca1b01b35c1c8c2aafebf7
Ali <4e5.kn@mail.ru>
2025-04-06 01:18:33 +0300

    fix: cookie

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -14,21 +14,27 @@ async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
 
   const allowedOrigins = ['http://localhost:3000', 'https://joyfy.online'];
-
   app.enableCors({
-    origin: (
-      origin: string | undefined,
-      callback: (error: Error | null, origin?: string | boolean) => void,
-    ) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, origin);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  });
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization,X-Requested-With',  });
+
+
+  // app.enableCors({
+  //   origin: (
+  //     origin: string | undefined,
+  //     callback: (error: Error | null, origin?: string | boolean) => void,
+  //   ) => {
+  //     if (!origin || allowedOrigins.includes(origin)) {
+  //       callback(null, origin);
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  //   credentials: true,
+  // });
 
   const config = new DocumentBuilder()
     .setTitle(SWAGGER_TITLE)
