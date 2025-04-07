b90cb9c36643b349e9df15a1e65ceac156dc1114
Ali <4e5.kn@mail.ru>
2025-04-07 23:44:28 +0300

    fix: clear cookie

---
+++ apps/posts/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { PostsModule } from './posts.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(PostsModule);
+  app.setGlobalPrefix('/api/v2');
+  await app.listen(process.env.port ?? 3000);
+}
+
+void bootstrap();
