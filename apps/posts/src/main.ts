a1f88c6d7ff2ec6eb2163ec6a4dd8b2f8ea3c467
Ali <4e5.kn@mail.ru>
2025-04-09 01:05:22 +0300

    fix: add test

--- apps/posts/src/main.ts
+++ apps/posts/src/main.ts
@@ -3,8 +3,8 @@ import { PostsModule } from './posts.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PostsModule);
-  app.setGlobalPrefix('/api/v1');
-  await app.listen(process.env.port ?? 3000);
+  // app.setGlobalPrefix('/api/v1');
+  await app.listen(process.env.PORT ?? 3022);
 }
 
 void bootstrap();
