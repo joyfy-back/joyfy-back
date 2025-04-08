2dfc9e6863ca516a323e18c23df20d95cc772613
Ali <4e5.kn@mail.ru>
2025-04-09 00:44:02 +0300

    fix: add test

--- apps/posts/test/app.e2e-spec.ts
+++ apps/posts/test/app.e2e-spec.ts
@@ -19,6 +19,6 @@ describe('FilesController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('salasas');
+      .expect('salasas!');
   });
 });
