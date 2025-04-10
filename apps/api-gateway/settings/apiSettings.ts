0385db5240237fdedfda80941b0ce9f89ec2299e
Ali <4e5.kn@mail.ru>
2025-04-10 06:27:28 +0300

    fix: settings

--- apps/api-gateway/settings/apiSettings.ts
+++ apps/api-gateway/settings/apiSettings.ts
@@ -2,8 +2,11 @@ import { IsNumber } from 'class-validator';
 import { EnvironmentVariable } from './configuration';
 
 export class ApiSettings {
-  constructor(private environmentVariables: EnvironmentVariable) {}
+  constructor(private environmentVariables: EnvironmentVariable) { }
   @IsNumber()
   PORT: number = Number(this.environmentVariables.PORT);
   SECRET: number = Number(this.environmentVariables.SECRET);
+  GOOGLE_CLIENT_ID: string = String(this.environmentVariables.GOOGLE_CLIENT_ID);
+  GOOGLE_CLIENT_SECRET: string = String(this.environmentVariables.GOOGLE_CLIENT_SECRET)
+  RECAPTCHA_SECRET_KEY: string = String(this.environmentVariables.RECAPTCHA_SECRET_KEY)
 }
