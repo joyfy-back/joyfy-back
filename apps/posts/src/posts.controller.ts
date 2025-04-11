93fc5a6aa19c6a1b6c4033ad95927087dc798ec2
Ali <4e5.kn@mail.ru>
2025-04-11 15:42:23 +0300

    fix: add url /app

--- apps/posts/src/posts.controller.ts
+++ apps/posts/src/posts.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { PrismaClient } from '@prisma/client';
+import { get } from 'http';
 
 @Controller()
 export class PostsController {
-  constructor(private readonly postsService:PostsService) {}
+  constructor(private readonly postsService: PostsService, private prisma: PrismaClient) { }
 
   @Get()
   getHello(): string {
     return this.postsService.getHello();
+  }
+  @Get('/app')
+  getlo(): Promise<{ postId: string; username: string; createdAt: Date; updatedAt: Date } | null> {
+    return this.prisma.posts.findUnique({
+      where: { postId: '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed' }
+    });
   }
 }
