7217b3372cd77aa7f90d0025efe6d7619b4dfeae
Ali <4e5.kn@mail.ru>
2025-04-12 12:56:49 +0300

    fix: add comments

--- apps/posts/src/posts/controllers/posts.controllers.ts
+++ apps/posts/src/posts/controllers/posts.controllers.ts
@@ -17,4 +17,11 @@ export class PostsController {
       where: { postId: '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed' }
     });
   }
+
+  @Get('/comments')
+  async  getloo() {
+    return await this.prisma.comment.findUnique({
+      where: { commetId: '1b9d6bcd-bbfd-4b2d-9b5d-ab8hlbbd4bed' }
+    });
+  }
 }
