2244935e2f509319be72f76a0bc05de36ef77144
pavvelpotapov <pavelminsk1979@mail.ru>
2025-04-17 02:41:10 +0300

    JOY-60 ADD-POST feat: add post/path4 prisma cletnt

--- apps/content/src/posts/infrastructure/post-repository.ts
+++ apps/content/src/posts/infrastructure/post-repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../../../../api-gateway/prisma/prisma.service';
+import { PrismaService } from '../../../prisma/prisma.service';
 import { AddPost } from '../types/post-types';
 import { Posts } from '../../../prisma/generated/prisma-client-content';
 
